20e46c81c3054cbffb41891c0ff2085043fda251
João Victor <joao.dipaay@gmail.com>
2024-05-01 15:31:35 -0300

    configuração inicial

---
+++ src/main/java/br/com/hyzed/hyzedapi/HyzedApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hyzed.hyzedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyzedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyzedApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/hyzed/hyzedapi/HyzedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hyzed.hyzedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyzedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
