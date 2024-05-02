4f423dd5dc11271184d03e27191a491106527b83
João Victor <joao.dipaay@gmail.com>
2024-05-01 22:18:54 -0300

    fix: userdetailsservice lista de autorizações

--- src/main/java/br/com/hyzed/hyzedapi/infra/security/CustomUserDetailsService.java
+++ src/main/java/br/com/hyzed/hyzedapi/infra/security/CustomUserDetailsService.java
@@ -6,8 +6,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.ArrayList;
-
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
@@ -19,6 +17,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user.getAuthorities());
     }
 }
