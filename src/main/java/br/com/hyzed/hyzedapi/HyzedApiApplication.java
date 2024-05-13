e584505864346c1b83dd7ccdf02a2910bb11b018
João Victor Novais <joao.dipaay@gmail.com>
2024-05-13 15:21:40 -0300

    docs: documentação com swagger

--- src/main/java/br/com/hyzed/hyzedapi/HyzedApiApplication.java
+++ src/main/java/br/com/hyzed/hyzedapi/HyzedApiApplication.java
@@ -1,9 +1,12 @@
 package br.com.hyzed.hyzedapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "hyzed-api", version = "1.0", description = "Ecommerce API for hyzedwear"))
 public class HyzedApiApplication {
 
     public static void main(String[] args) {
