d5139b363452f0fefb65dedaa7b7d1551541e52b
João Victor Novais <joao.dipaay@gmail.com>
2024-05-13 15:07:57 -0300

    docs: OrderController

--- src/main/java/br/com/hyzed/hyzedapi/controllers/OrderController.java
+++ src/main/java/br/com/hyzed/hyzedapi/controllers/OrderController.java
@@ -4,6 +4,9 @@ import br.com.hyzed.hyzedapi.domain.order.Order;
 import br.com.hyzed.hyzedapi.domain.order.OrderStatus;
 import br.com.hyzed.hyzedapi.domain.order.OrderStatusDTO;
 import br.com.hyzed.hyzedapi.services.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -20,6 +23,12 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Operation(description = "Atualiza o Status de um pedido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status de pedido alterado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Pedido não encontrado")
+    })
     @PutMapping("/{id}/status")
     public ResponseEntity<Order> changeOrderStatus(@PathVariable String id, @RequestBody @Valid OrderStatusDTO orderStatus) {
         orderService.changeOrderStatus(id, orderStatus);
