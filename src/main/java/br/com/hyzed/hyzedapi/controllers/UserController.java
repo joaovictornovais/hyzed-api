b7bf492da4b4bd01d655bc54e5d317583b9b9ce2
João Victor Novais <joao.dipaay@gmail.com>
2024-05-13 15:14:31 -0300

    docs: UserController

--- src/main/java/br/com/hyzed/hyzedapi/controllers/UserController.java
+++ src/main/java/br/com/hyzed/hyzedapi/controllers/UserController.java
@@ -7,6 +7,9 @@ import br.com.hyzed.hyzedapi.domain.user.User;
 import br.com.hyzed.hyzedapi.domain.user.UserMinDTO;
 import br.com.hyzed.hyzedapi.services.OrderService;
 import br.com.hyzed.hyzedapi.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,16 +29,35 @@ public class UserController {
         this.orderService = orderService;
     }
 
+    @Operation(description = "Retorna as informações minimas de um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+        }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UserMinDTO> getUserMinInfo(@PathVariable String id) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserMinInfo(id));
     }
 
+    @Operation(description = "Cria uma ordem de pedido para o usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ordem de pedido criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+        }
+    )
     @PostMapping("/{id}/orders")
     public ResponseEntity<Order> createOrder(@PathVariable String id, @RequestBody @Valid ProductsDTO productsDTO) {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.create(id, productsDTO));
     }
 
+    @Operation(description = "Retorna todas as ordens de pedidos de um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+        }
+    )
     @GetMapping("/{id}/orders")
     public ResponseEntity<List<Order>> getOrders(@PathVariable String id) {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getOrdersByUser(id));
